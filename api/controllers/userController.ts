ba3d4518c5ba011522164c64622a205d49dc9d14
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-04-18 15:46:56 +0400

    get all users & get single user

--- api/controllers/userController.ts
+++ api/controllers/userController.ts
@@ -1,10 +1,18 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import User from "../models/User";
+import { NotFoundError } from "../errors";
 
 export const getAllUsers = async (req: Request, res: Response) => {
-  res.send("get all users");
+  const users = await User.find({ role: "user" }).select("-password");
+  res.status(StatusCodes.OK).json({ users });
 };
 export const getSingleUser = async (req: Request, res: Response) => {
-  res.send("get Signle users");
+  const user = await User.findOne({ _id: req.params.id }).select("-password");
+  if (!user) {
+    throw new NotFoundError(`No user with id: ${req.params.id}`);
+  }
+  res.status(StatusCodes.OK).json({ user });
 };
 export const showCurrentUser = async (req: Request, res: Response) => {
   res.send("show current users");
--- dist/api/controllers/userController.js
+++ dist/api/controllers/userController.js
@@ -8,14 +8,25 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.updateUserPassword = exports.updateUser = exports.showCurrentUser = exports.getSingleUser = exports.getAllUsers = void 0;
+const http_status_codes_1 = require("http-status-codes");
+const User_1 = __importDefault(require("../models/User"));
+const errors_1 = require("../errors");
 const getAllUsers = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    res.send("get all users");
+    const users = yield User_1.default.find({ role: "user" }).select("-password");
+    res.status(http_status_codes_1.StatusCodes.OK).json({ users });
 });
 exports.getAllUsers = getAllUsers;
 const getSingleUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    res.send("get Signle users");
+    const user = yield User_1.default.findOne({ _id: req.params.id }).select("-password");
+    if (!user) {
+        throw new errors_1.NotFoundError(`No user with id: ${req.params.id}`);
+    }
+    res.status(http_status_codes_1.StatusCodes.OK).json({ user });
 });
 exports.getSingleUser = getSingleUser;
 const showCurrentUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
